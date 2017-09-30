97bf1ddab608803cb03ab46950f6f0a5177691de
Tinus Flagstad <tinus@flagstad.as>
2017-09-30 12:17:13 +0200

    Added a bit more info and cleaned up the code a little

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let Typer={
+let Typer = {
 console: null,
 text: null,
-accessCountimer:null,
-index:0, // current cursor position
-speed:2, // speed of the Typer
-file:"", // file
-	init: function(){
-		accessCountimer=setInterval(function(){Typer.blinker();},500); // inizialize timer for blinking cursor
+accessCountimer: null,
+index: 0, // current cursor position
+speed: 2, // speed of the Typer
+file: "", // file
+	init: function() {
+		accessCountimer = setInterval(function(){Typer.blinker();},500); // inizialize timer for blinking cursor
 		let xmlhttp, text;
 		xmlhttp = new XMLHttpRequest();
 		xmlhttp.open('GET', Typer.file, false);
@@ -14,28 +14,28 @@ file:"", // file
 		Typer.text = xmlhttp.responseText;
 	},
 
-	content: function(){
+	content: function() {
 		return document.getElementById("console").innerHTML;
 	},
 
-	addText: function(){// Main function to add the code
-		if(Typer.text){ // if text is loaded
-			let cont=Typer.content(); // get the console content
-			if(cont.substring(cont.length-1,cont.length)=="|") {// if the last char is the blinking cursor
-				this.console.innerHTML = this.console.innerHTML.replace('|',''); // remove it before adding the text
+	addText: function() {// Main function to add the code
+		if(Typer.text) { // if text is loaded
+			let cont = Typer.content(); // get the console content
+			if(cont.substring(cont.length - 1, cont.length) == "|") {// if the last char is the blinking cursor
+				this.console.innerHTML = this.console.innerHTML.replace('|', ''); // remove it before adding the text
 			}
-			Typer.index+=Typer.speed;	// add to the index the speed
-			let text=Typer.text.substring(0,Typer.index)// parse the text for stripping html enities
-			let rtn= new RegExp("\n", "g"); // newline regex
-			text = text.replace(rtn,"<br>");
+			Typer.index += Typer.speed;	// add to the index the speed
+			let text = Typer.text.substring(0, Typer.index)// parse the text for stripping html enities
+			let rtn = new RegExp("\n", "g"); // newline regex
+			//text = text.replace(rtn,"<br>");
 			this.console.innerHTML = text;
 		}
 	},
 
-	blinker: function(){ // blinking cursor
-		let cont=this.content();
-		if(cont.substring(cont.length-1,cont.length)=="|") {// if last char is the cursor
-			this.console.innerHTML = this.console.innerHTML.replace('|','');
+	blinker: function() { // blinking cursor
+		let cont = this.content();
+		if(cont.substring(cont.length - 1, cont.length)=="|") {// if last char is the cursor
+			this.console.innerHTML = this.console.innerHTML.replace('|', '');
 		}
 		else {
 			this.console.innerHTML += "|";
@@ -43,8 +43,8 @@ file:"", // file
 	}
 }
 
-Typer.speed=3;
-Typer.file="tinusf.txt";
+Typer.speed = 3;
+Typer.file = "tinusf.txt";
 Typer.console = document.getElementById("console");
 Typer.init();
  
